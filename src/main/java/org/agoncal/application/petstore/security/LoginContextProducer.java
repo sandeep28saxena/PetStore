6ed3452d170503b161d150c955049fa4abf3c7b0
Bruce the Awesome Hero <Brucee@Bruce-Pannamans-MacBook.local>
2014-07-16 11:27:05 +0100

    Retort to James
    (̿▀̿ ̿Ĺ̯̿̿▀̿ ̿)̄ MY NAME IS DONG,JAMES DONG (̿▀̿ ̿Ĺ̯̿̿▀̿ ̿)̄

--- src/main/java/org/agoncal/application/petstore/security/LoginContextProducer.java
+++ src/main/java/org/agoncal/application/petstore/security/LoginContextProducer.java
@@ -19,7 +19,7 @@ public class LoginContextProducer {
     // ======================================
     // =             Attributes             =
     // ======================================
-
+//this injects a callback handler
     @Inject
     private SimpleCallbackHandler callbackHandler;
 
