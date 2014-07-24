ad2404fcda2037bb225610e517cee23bf4e8eef4
CowCowGarcia <cowcowgarcia@gmail.com>
2014-07-24 11:04:04 +0100

    added comments for the stock class

--- src/main/java/org/agoncal/application/petstore/domain/Stock.java
+++ src/main/java/org/agoncal/application/petstore/domain/Stock.java
@@ -8,7 +8,10 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
-
+/*this class gives products a stock to regulate orders. Prevents having
+millions and millions of Bulldogs being ordered for example. 
+Also will help the website actually become functional as a shop
+*/
 @Entity
 @NamedQueries({
     @NamedQuery(name = Stock.SEARCH, query = "SELECT i FROM Stock i WHERE i.item.id LIKE :keyword ORDER BY i.item.id")
@@ -17,29 +20,34 @@ import javax.xml.bind.annotation.XmlTransient;
 @XmlRootElement
 
 public class Stock {
+//makes Stock a foreign key to product
 @Id
 @OneToOne
 @JoinColumn(name="prod_fk", nullable=false)
 @XmlTransient
 private Item item;
 
+//stock must be an integer, and must be provided.
 @Column(nullable=false)
 private int stockLevel;
 
+//an integer for stock sold. Used for algorithm to see if a product is available
 @Column(nullable=false)
 private int stockSold;
 public static final String SEARCH = "Stock.search";
 
+
+
 public Stock(){
 	
 }
-
+//constructor for Stock
 public Stock(Item item, int stockLevel, int stockSold){
 	this.item=item;
 	this.stockLevel=stockLevel;
 	this.stockSold=stockSold;
 }
-
+//the getters and setters are needed for Stock.
 public Item getItem() {
 	return item;
 }
@@ -72,6 +80,7 @@ public boolean equals(Object o) {
     return true;
 }
 @Override
+//the String compiler for when we want to print out values corresponding with Stock.
 public String toString() {
     final StringBuilder sb = new StringBuilder();
     sb.append("Stock");
